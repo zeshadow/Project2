e3be3300fbe482b6e28a57f0de0ae6dd3cb22b6c
Michael Haderman <mhaderman@gmail.com>
2016-11-09 15:04:22 -0800

    initial commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var school = require('./routes/school_routes');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/school', school);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
